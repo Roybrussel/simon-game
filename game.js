0342a4fe86d4a4fb744d76201dacc97dccc0711a
Roy-Arne Brussel <29131401+Roybrussel@users.noreply.github.com>
2021-01-11 17:45:26 +0100

    Finished stp 6

--- game.js
+++ game.js
@@ -9,6 +9,7 @@ $(".btn").click(function () {
   userClickedPattern.push(userChosenColour);
   var audio = new Audio(`sounds/${userChosenColour}.mp3`);
   audio.play();
+  animatePress(userChosenColour);
 });
 
 function nextSequence() {
@@ -18,4 +19,11 @@ function nextSequence() {
   $(`#${randomChosenColour}`).fadeOut(50).fadeIn(50);
   var audio = new Audio(`sounds/${randomChosenColour}.mp3`);
   audio.play();
+}
+
+function animatePress(currentColour) {
+  $(`.${currentColour}`).addClass("pressed");
+  setTimeout(function () {
+    $(`.${currentColour}`).removeClass("pressed");
+  }, 100);
 }
