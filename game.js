1313979f56d54318fda4650bfdd3933670e7bb0e
Roy-Arne Brussel <29131401+Roybrussel@users.noreply.github.com>
2021-01-11 20:25:13 +0100

    Finished step 9 and therefor completed the game.

--- game.js
+++ game.js
@@ -19,15 +19,15 @@ $(document).keydown(function () {
 });
 
 $(".btn").click(function () {
-  var userChosenColour = this.id;
+  var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
-  var audio = new Audio(`sounds/${userChosenColour}.mp3`);
-  audio.play();
+  playSound(userChosenColour);
   animatePress(userChosenColour);
-  checkAnswer(userClickedPattern.length);
+  checkAnswer(userClickedPattern.length - 1);
 });
 
 function nextSequence() {
+  userClickedPattern = [];
   level++;
   $("#level-title").text("Level " + level);
   var randomNumber = Math.floor(Math.random() * 4);
@@ -46,17 +46,32 @@ function animatePress(currentColour) {
 }
 
 function checkAnswer(currentLevel) {
-  if (currentLevel === gamePattern.length) {
-    setTimeout(function () {
-      nextSequence();
-    }, 1000);
-    userClickedPattern = [];
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
   } else {
-    gameover.play();
+    playSound("wrong");
     $("body").addClass("game-over");
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+
     setTimeout(function () {
       $("body").removeClass("game-over");
-    }, 100);
-    $("h1").text("Game Over, Press Any Key to Restart");
+    }, 200);
+
+    startOver();
   }
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
+}
+
+function playSound(name) {
+  var audio = new Audio(`sounds/${name}.mp3`);
+  audio.play();
 }
